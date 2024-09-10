63e5a7027b7aeda557cbdb930eede30e5368fa07
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-09-10 14:14:30 -0400

    feat: tailwind config colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        slater: "#0d1521",
+        primary: "#50cf61",
+        secondary: "#98f28c",
+      },
+    },
+  },
+  plugins: [],
+};
