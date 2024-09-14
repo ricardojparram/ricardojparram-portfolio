39fc84589e148ea4156fb8a77890294932679b45
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-09-13 21:56:12 -0400

    feat: new principal text-color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,7 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        slater: "#0d1521",
-        primary: "#50cf61",
-        secondary: "#98f28c",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
