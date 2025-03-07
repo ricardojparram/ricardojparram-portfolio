78727151016de3372e56fa14e0fb1f069e8e9ac4
Ricardo Parra <ricardo.prxr16@gmail.com>
2025-03-06 22:03:00 -0400

    feat: CSS and Jquery icons added

--- src/lib/technologies.ts
+++ src/lib/technologies.ts
@@ -6,6 +6,8 @@ import MySQL from "../components/icons/MySQL.astro";
 import Bootstrap from "../components/icons/Bootstrap.astro";
 import GraphQL from "../components/icons/GraphQL.astro";
 import Expo from "../components/icons/Expo.astro";
+import CSS from "../components/icons/CSS.astro";
+import JQuery from "../components/icons/JQuery.astro";
 export const Technologies = {
   Tailwind,
   Astro,
@@ -15,5 +17,7 @@ export const Technologies = {
   Bootstrap,
   Expo,
   GraphQL,
+  CSS,
+  JQuery,
 };
 export type TechnologiesType = keyof typeof Technologies;
